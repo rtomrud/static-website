8f4e3b3003ef471dcc4ea855916d409d96d498ea
Artem Rudenko <rtomrud@gmail.com>
2025-01-15 23:01:09 +0100

    Add ESLint

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  { ignores: ["dist"] },
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: 2023,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+    },
+  },
+];
